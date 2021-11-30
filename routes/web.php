310d9dc2e60ff1f394394f5387affda548a29d89
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-11-30 21:35:42 +0100

    Create all orders

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-/**
- * Path to find posts
- */
-Route::resource('post', [PostController::class]);
