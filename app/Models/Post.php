81aab2b36ce619530a6d366e63044a9d8d73cfe1
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-11-30 20:42:48 +0100

    Add the protected && hidden attributes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
