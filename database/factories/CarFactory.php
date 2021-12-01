125bf780832d1ca6460f2b5c39adfdb9e86bf458
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-12-01 23:05:23 +0100

    Add factory to cars

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Car;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CarFactory extends Factory
+{
+
+    protected $model = Car::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'patent' => $this->faker->uuid,
+            'user_id' => User::factory()->create()->id
+        ];
+    }
+}
